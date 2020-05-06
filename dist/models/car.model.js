e6c8e625aa1154216da68359a22c667cf3bfa823
simbrar20 <simranbrar20@mail.fresnostate.edu>
2020-05-05 20:14:18 -0700

    finished get endpoints for car model

--- dist/models/car.model.js
+++ dist/models/car.model.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 class Car {
     constructor(norm) {
@@ -7,6 +15,7 @@ class Car {
                 make: { type: String, maxlength: 24 },
                 model: { type: String, maxlength: 24 },
                 year: { type: String, maxlength: 24 },
+                mileage: { type: String, maxlength: 24 },
                 user_id: {
                     type: Number,
                     key: 'foreign',
@@ -14,7 +23,44 @@ class Car {
                     onDelete: 'cascade',
                     onUpdate: 'cascade'
                 },
-            }, 'A table to store user car model', []];
+            }, 'A table to store user car model',
+            [
+                {
+                    route: '/get-all-cars',
+                    method: 'POST',
+                    callback: this.getAllCars,
+                    requireToken: true,
+                },
+                {
+                    route: '/get-car-by-id/:id',
+                    method: 'POST',
+                    callback: this.getCarById,
+                    requireToken: true,
+                }
+            ]];
+    }
+    getAllCars(model) {
+        return (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+            req.body = {
+                get: ['*']
+            };
+            let carCtrl = model.controller;
+            let resp = yield carCtrl.get(req, null, null);
+            res.json({ message: 'from Success', resp });
+        });
+    }
+    getCarById(model) {
+        return (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+            req.body = {
+                get: ['*'],
+                where: {
+                    id: req.params.id
+                }
+            };
+            let carCtrl = model.controller;
+            let resp = yield carCtrl.get(req, null, null);
+            res.json({ message: 'Success', resp });
+        });
     }
     set model(model) {
         this._model = model;
--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,27 +1,69 @@
+import {Request, Response, NextFunction} from 'express';
+
 export class Car {
-    _model: any;
-    constructor(norm: any) {
-      this.model = [{
-        id: { type: Number, key: 'primary' },
-        make: { type: String, maxlength: 24 },
-        model: { type: String, maxlength: 24 },
-        year: { type: String, maxlength: 24 },
-        user_id: {
-          type: Number,
-          key: 'foreign',
-          references: { table: 'User', foreignKey: 'id' },
-          onDelete: 'cascade',
-          onUpdate: 'cascade'
-        },
-      }, 'A table to store user car model', []];
-    }
-  
-    set model(model: any) {
-      this._model = model;
-    }
-  
-    get model() {
-      return this._model;
-    }
-  
+  _model: any;
+  constructor(norm: any) {
+    this.model = [{
+      id: { type: Number, key: 'primary' },
+      make: { type: String, maxlength: 24 },
+      model: { type: String, maxlength: 24 },
+      year: { type: String, maxlength: 24 },
+      mileage: { type: String, maxlength: 24 },
+      user_id: {
+        type: Number,
+        key: 'foreign',
+        references: { table: 'User', foreignKey: 'id' },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+    }, 'A table to store user car model',
+    [
+      {
+        route: '/get-all-cars',
+        method: 'POST',
+        callback: this.getAllCars,
+        requireToken: true,
+      },
+      {
+        route: '/get-car-by-id/:id',
+        method: 'POST',
+        callback: this.getCarById,
+        requireToken: true,
+      }
+    ]];
   }
+
+  getAllCars(model: any) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        req.body = {
+          get: ['*']
+      }
+      let carCtrl = model.controller;
+      let resp = await carCtrl.get(req, null, null);
+      res.json({ message: 'from Success', resp });
+    }
+  }
+
+  getCarById(model: any) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        req.body = {
+          get: ['*'],
+          where: {
+            id: req.params.id
+          }
+      }
+      let carCtrl = model.controller;
+      let resp = await carCtrl.get(req, null, null);
+      res.json({ message: 'Success', resp });
+    }
+  }
+
+  set model(model: any) {
+    this._model = model;
+  }
+
+  get model() {
+    return this._model;
+  }
+
+}
