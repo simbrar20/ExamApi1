9b6c577d2816db3f418bbcf21d6c5912f46cfb7f
simbrar20 <simranbrar20@mail.fresnostate.edu>
2020-05-06 20:30:35 -0700

    finish post response for car model

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -1,5 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    getAllCars(model: any): (req: Request<import("../../../../../../Users/admin/Desktop/ExamAPI1/node_modules/@types/express-serve-static-core").ParamsDictionary, any, any, import("../../../../../../Users/admin/Desktop/ExamAPI1/node_modules/@types/express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<void>;
+    getCarById(model: any): (req: Request<import("../../../../../../Users/admin/Desktop/ExamAPI1/node_modules/@types/express-serve-static-core").ParamsDictionary, any, any, import("../../../../../../Users/admin/Desktop/ExamAPI1/node_modules/@types/express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<void>;
     model: any;
 }
